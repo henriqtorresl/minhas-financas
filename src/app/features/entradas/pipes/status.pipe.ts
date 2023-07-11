3cc5905d103e58978b481a6102f1f6eda8435afa
henriqtorresl <henriquetlandin@gmail.com>
2023-07-11 12:34:53 -0300

    lógica do pipe

--- src/app/features/entradas/pipes/status.pipe.ts
+++ src/app/features/entradas/pipes/status.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class StatusPipe implements PipeTransform {
 
   transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    return value ? 'Pago' : 'Pendente';   // se value == true, return 'Pago', se for false return 'Pendente'
   }
 
 }
