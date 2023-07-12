d95f97ff66f87b0bd198fa851629be8418de7e9a
henriqtorresl <henriquetlandin@gmail.com>
2023-07-12 12:34:42 -0300

    alterações no service

--- src/app/features/dashboard/service/dashboard.service.ts
+++ src/app/features/dashboard/service/dashboard.service.ts
@@ -13,8 +13,12 @@ export class DashboardService extends HttpBaseService {
     super(injector);    // chama o construtor da classe pai, e na classe pai o construtor recebe um injector
   }
 
-  getEntradas(): Observable<any> {
-    return this.httpGet(this.endpoint);     // chamando o método da classe pai..
+  getEntradas(payload?: any): Observable<any> {
+
+    const params = payload ? `?q=${payload.mes}/${payload.ano}` : '';
+
+    return this.httpGet(`${this.endpoint}${params}`);     // chamando o método da classe pai..
+    // dessa forma eu tenho uma função generica: se eu nn passar nenhum parametro nn acontece nd e se eu passar ela filtra pelos parametros...
   }
 
 }
