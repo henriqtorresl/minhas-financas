5484502c5a971aa1f611b0b5d53772bc5c455cd9
henriqtorresl <henriquetlandin@gmail.com>
2023-08-11 11:45:33 -0300

    configuração das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)},   // Lazyload do módulo de dashboard
   {path: 'categorias', loadChildren: () => import('./features/categorias/categorias.module').then(m => m.CategoriasModule)},   // Lazyload do módulo de cateforias
-  {path: 'entradas', loadChildren: () => import('./features/entradas/entradas.module').then(m => m.EntradasModule)}   // Lazyload do módulo de cateforias
+  {path: 'entradas', loadChildren: () => import('./features/entradas/entradas.module').then(m => m.EntradasModule)},   // Lazyload do módulo de cateforias
+  {path: 'auth', loadChildren: () => import('./commom/auth/auth.module').then(m => m.AuthModule)} // Lazyload do módulo de autenticação (auth)
 ];
 
 @NgModule({
--- src/app/commom/auth/auth-routing.module.ts
+++ src/app/commom/auth/auth-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent,
+  },
+  {
+    path: 'cadastro', component: CadastroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
